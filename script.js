328b37cb144506c2b9bc7c046ed0dbc41dfbf40a
SathiraSS <sathirassg12@gmail.com>
2023-03-28 20:38:16 +0530

    Arrays
    Character Arrays Created

---
+++ script.js
@@ -0,0 +1,18 @@
+let btn = document.getElementById("btn");
+let input = document.getElementById("input");
+
+// Uppercase letters array
+let uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+// Lowercase letters array
+let lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+ 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Numbers array
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Symbols array
+let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=','.', '<', '>', '?'];
+
+
