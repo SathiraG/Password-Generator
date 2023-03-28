718117c331ac92df138fb32a7608bc6f657db5c8
SathiraSS <sathirassg12@gmail.com>
2023-03-28 21:11:18 +0530

    Password Generating Part
    Password Generating Part is successfully Finished

--- script.js
+++ script.js
@@ -15,4 +15,33 @@ let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 // Symbols array
 let symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '_', '-', '+', '=','.', '<', '>', '?'];
 
+let password = ""
+
+btn.addEventListener("click", function Generate(){
+
+    let characterCount = input.value;
+    for (let i = 0; i < characterCount ; i++){
+        
+
+        let arrayNo = Math.floor(Math.random() * 4)
+        if(arrayNo == 0){
+            let x = Math.floor(Math.random() * uppercaseLetters.length)
+            password += uppercaseLetters[x]
+        }else if(arrayNo == 1){
+            x = Math.floor(Math.random() * lowercaseLetters.length)
+            password += lowercaseLetters[x]
+        }else if (arrayNo == 2){
+            x = Math.floor(Math.random() * numbers.length)
+            password += numbers[x]
+        }else{
+            x = Math.floor(Math.random() * symbols.length)
+            password += symbols[x]
+
+        }
+
+        console.log(password)
+    }
+    console.log("The Password is : " + password)
+    
+})
 
